a47586213e167ce722975dac6ec19ea54b8c09c7
Pouya Aklion <pouyaakn4@gmail.com>
2020-07-26 21:45:46 +0430

    add express and webhooks

--- bot.js
+++ bot.js
@@ -1,32 +1,47 @@
-const Promise = require('bluebird');
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express')
+const bodyParser = require('body-parser');
+
 const { User } = require('./User');
 const env = require('dotenv').config()
 
-const token = process.env.TOKEN;
-const bot = new TelegramBot(token, { polling: true });
-const opts = {
+
+
+const token = process.env.TELEGRAM_TOKEN;
+let bot;
+
+const commands = {
+  btnFilters: 'فیلتر ها',
+  btnPremium : 'افزایش اعتبار',
+  btnSharePhone: 'ارسال شماره موبایل',
+  btnShareLocation: 'ارسال موقعیت',
+}
+const keyboard = {
   reply_markup: JSON.stringify({
-    keyboard: [[{ text: 'share phone number', request_contact: true }, 'B'], ['D'], ['E', { text: 'share location', request_location: true }]],
+    keyboard: [[commands.btnFilters]],
     resize_keyboard: true
   })
 };
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1];
-  bot.sendMessage(chatId, resp);
-});
-bot.onText(/\/start/, async (msg, match) => {
 
-  let user = await User.findOne({ id: msg.from.id })
+
+
+if (process.env.NODE_ENV === 'production') {
+   bot = new TelegramBot(token);
+   bot.setWebHook(process.env.HEROKU_URL + bot.token);
+} else {
+   bot = new TelegramBot(token, { polling: true });
+}
+
+bot.onText(/\/start/, async (msg, match) => {
+  let user =  await checkUser(msg.from.id)
   if (!user) {
     await registerUser(msg.from)
-    bot.sendMessage(msg.chat.id, 'welcome', opts)
+    bot.sendMessage(msg.chat.id, 'welcome', keyboard)
   } else if (user.requestCount >= 10) {
     bot.sendMessage(msg.chat.id, 'حساب رایگان شما تمام شده است');
   } else {
-    bot.sendMessage(msg.chat.id, 'welcome again', opts)
+    bot.sendMessage(msg.chat.id, 'welcome again', keyboard)
     user.requestCount++;
     await user.save();
 
@@ -36,21 +51,39 @@ bot.onText(/\/start/, async (msg, match) => {
 
 
 bot.on('message', (msg) => {
-
+  if(msg.text === commands.btnFilters){
+    console.log(msg)
+  }
 });
 
-function timeout(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+// function timeout(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
 async function registerUser(user) {
   let newUser = new User({
     id: user.id,
     username: user.username || null,
-    fullname: user.first_name || null
+    fullname: user.first_name || null + user.last_name || null
   })
   await newUser.save().catch(e => {
     return false
   });
   return true;
 }
+async function checkUser(id) {
+  return await User.findOne({id}) || false;
+
+}
+
+
+const app = express();
+ 
+app.use(bodyParser.json());
+ 
+app.listen(process.env.PORT);
+ 
+app.post('/' + bot.token, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
