9cedffc43bb30e5ddc146cacfa0ad849dab7e6bc
Pouya Aklion <pouyaakn4@gmail.com>
2020-07-26 22:43:17 +0430

    add server listen in function

--- bot.js
+++ bot.js
@@ -5,14 +5,12 @@ const bodyParser = require('body-parser');
 const { User } = require('./User');
 const env = require('dotenv').config()
 
-
-
 const token = process.env.TELEGRAM_TOKEN;
 let bot;
 
 const commands = {
   btnFilters: 'فیلتر ها',
-  btnPremium : 'افزایش اعتبار',
+  btnPremium: 'افزایش اعتبار',
   btnSharePhone: 'ارسال شماره موبایل',
   btnShareLocation: 'ارسال موقعیت',
 }
@@ -23,18 +21,48 @@ const keyboard = {
   })
 };
 
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function registerUser(user) {
+  let newUser = new User({
+    id: user.id,
+    username: user.username || null,
+    fullname: user.first_name || null + user.last_name || null
+  })
+  await newUser.save().catch(e => {
+    return false
+  });
+  return true;
+}
+async function checkUser(id) {
+  return await User.findOne({ id }) || false;
+
+}
+
+function listenWebhooks() {
+  const app = express();
+  app.use(bodyParser.json());
+  app.listen(process.env.PORT);
+  app.post('/' + bot.token, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
+}
+
 
 
 
 if (process.env.NODE_ENV === 'production') {
-   bot = new TelegramBot(token);
-   bot.setWebHook(process.env.HEROKU_URL + bot.token);
+  bot = new TelegramBot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
 } else {
-   bot = new TelegramBot(token, { polling: true });
+  bot = new TelegramBot(token, { polling: true });
 }
 
 bot.onText(/\/start/, async (msg, match) => {
-  let user =  await checkUser(msg.from.id)
+  let user = await checkUser(msg.from.id)
   if (!user) {
     await registerUser(msg.from)
     bot.sendMessage(msg.chat.id, 'welcome', keyboard)
@@ -51,39 +79,9 @@ bot.onText(/\/start/, async (msg, match) => {
 
 
 bot.on('message', (msg) => {
-  if(msg.text === commands.btnFilters){
+  if (msg.text === commands.btnFilters) {
     console.log(msg)
   }
 });
 
-// function timeout(ms) {
-//   return new Promise(resolve => setTimeout(resolve, ms));
-// }
-
-async function registerUser(user) {
-  let newUser = new User({
-    id: user.id,
-    username: user.username || null,
-    fullname: user.first_name || null + user.last_name || null
-  })
-  await newUser.save().catch(e => {
-    return false
-  });
-  return true;
-}
-async function checkUser(id) {
-  return await User.findOne({id}) || false;
-
-}
-
-
-const app = express();
- 
-app.use(bodyParser.json());
- 
-app.listen(process.env.PORT);
- 
-app.post('/' + bot.token, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
+listenWebhooks();
